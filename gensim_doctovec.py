fde93edc7a4daeeba1325af51ebb43932ee38030
Anirudh Saraiya <anirudhmech911@gmail.com>
2018-02-25 03:55:32 +0530

    Update gensim_doctovec.py
    Code modified

--- gensim_doctovec.py
+++ gensim_doctovec.py
@@ -1,13 +1,8 @@
-# -*- coding: utf-8 -*-
-import gensim
+import gensim , logging
 import os
 import collections
 import smart_open
 import random
-
-test_data_dir = '{}'.format(os.sep).join([gensim.__path__[0], 'test', 'test_data'])
-lee_train_file = test_data_dir + os.sep + 'lee_background.cor'
-lee_test_file = test_data_dir + os.sep + 'lee.cor'
 
 
 def read_corpus(fname, tokens_only=False):
@@ -18,52 +13,48 @@ def read_corpus(fname, tokens_only=False):
             else:
                 # For training data, add tags
                 yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [i])
-          
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
             
+
+test_data_dir = '{}'.format(os.sep).join([gensim.__path__[0], 'test', 'test_data'])
+lee_train_file = test_data_dir + os.sep + 'lee_background.cor'
+lee_test_file = test_data_dir + os.sep + 'lee.cor'
+
 train_corpus = list(read_corpus(lee_train_file))
 test_corpus = list(read_corpus(lee_test_file, tokens_only=True))
 
+
+############ Model Train ################
+
+"""
 model = gensim.models.doc2vec.Doc2Vec(size=50, window = 50, min_count=2, iter=55)
 
 model.build_vocab(train_corpus)
+for epoch in range(5):
+    model.train(train_corpus,total_examples=len(train_corpus),epochs=100)
+    model.alpha -= 0.002 # decrease the learning rate
+    model.min_alpha = model.alpha # fix the learning rate, no deca
+    print("Epoch %s completed...." %(epoch))
 
-#print(model.wv.vocab['finance'].count )
-%time model.train(train_corpus, total_examples=model.corpus_count)
+model.save("gensim_model.model")
 
-ranks = []
-second_ranks = []
-for doc_id in range(len(train_corpus)):
-    inferred_vector = model.infer_vector(train_corpus[doc_id].words)
-    sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
-    rank = [docid for docid, sim in sims].index(doc_id)
-    ranks.append(rank)
-    
-    second_ranks.append(sims[1])
+"""
 
-collections.Counter(ranks)
+############ Model Test ################
 
-# doc_id = 299
-# print('Document ({}): «{}»\n'.format(doc_id, ' '.join(train_corpus[doc_id].words)))
-# print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
-# for label, index in [('MOST', 0), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
-#     print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
+model_loaded = gensim.models.doc2vec.Doc2Vec.load("gensim_model.model")
 
 
 # Pick a random document from the test corpus and infer a vector from the model
 doc_id = random.randint(0, len(test_corpus))
-inferred_vector = model.infer_vector(test_corpus[doc_id])
-sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+inferred_vector = model_loaded.infer_vector(test_corpus[doc_id])
+sims = model_loaded.docvecs.most_similar([inferred_vector])
+print(' '.join(test_corpus[doc_id]))
+print("\n")
+# print(model_loaded.docvecs.most_similar("hello there"))
+# docvec = model_loaded.infer_vector(test_corpus[0])
+# print(docvec)
 
-# Compare and print the most/median/least similar documents from the train corpus
-print('Test Document ({}): «{}»\n'.format(doc_id, ' '.join(test_corpus[doc_id])))
-print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
 for label, index in [('MOST', 0), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
-    print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
-
-
-
-
-
-
-
-
+    print(u'%s %s: %s\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
